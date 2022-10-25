3bdd8679c5c2f38840589a0cc1e98af8a1f69b45
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2022-10-25 14:35:29 +0200

    modal added

--- window-to-the-world/src/App.tsx
+++ window-to-the-world/src/App.tsx
@@ -5,23 +5,38 @@ import animals from './resources/animals/Animals';
 import {Howl, Howler} from 'howler';
 
 function App() {
+  interface CardItem {
+    image: string;
+    name: string;
+    sound: string;
+  }
+
   const [expanded, setExpanded] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState('');
+  const [modalName, setModalName] = useState('');
   const SoundPlay = (src: string) => {
     const sound = new Howl({src})
     sound.play();
   }
   Howler.volume(1.0);
-  const handleImageClick  = (sound : string) => { 
+  const handleImageClick  = (item : CardItem) => { 
     if(!expanded){
-      setTimeout(()=>{SoundPlay(sound)}, 200); //slight delay to give image time to expand
+      setTimeout(()=>{SoundPlay(item.sound)}, 200); //slight delay to give image time to expand
     }
     setExpanded(!expanded);
+    setModalOpen(!modalOpen);
+    setModalName(item.name);
+    setModalImage(item.image);
   }
   return(
     <div className='card-container'>
+      <div onClick={()=>{setModalOpen(!modalOpen); setExpanded(!expanded)}} className={modalOpen ? 'modal modal-open' : 'modal'}>
+        <img src={modalImage} alt={modalName} />
+      </div>
       {animals.map((item, index) => {
         return (
-          <Card key={index} image={item.image} altText= {item.name} handleClick = {()=>{handleImageClick(item.sound)}} />
+          <Card key={index} image={item.image} altText= {item.name} handleClick = {()=>{handleImageClick(item)}} />
           );
         })
       }
