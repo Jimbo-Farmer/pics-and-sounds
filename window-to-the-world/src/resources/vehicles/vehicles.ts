b391de6ab4200ad947245da77025a13c495c2454
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2022-11-01 11:22:17 +0100

    More vehicles

--- window-to-the-world/src/resources/vehicles/vehicles.ts
+++ window-to-the-world/src/resources/vehicles/vehicles.ts
@@ -1,14 +1,23 @@
 import CarImage from './images/car.jpg';
 import TrainImage from './images/train.jpg';
+import BikeImage from './images/bike.jpg';
+import HelicopterImage from './images/helicopter.jpg';
+import BoatImage from './images/boat.jpg';
 
 import CarSound from './sounds/car.mp3';
 import TrainSound from './sounds/train.mp3';
+import BikeSound from './sounds/bike.mp3';
+import HelicopterSound from './sounds/helicopter.mp3';
+import BoatSound from './sounds/boat.mp3';
 
 import {CardItem} from '../resourcesMaster';
 
 const vehicles: CardItem[] = [
   {name: 'car', image: CarImage, sound: CarSound},
   {name: 'train', image: TrainImage, sound: TrainSound},
+  {name: 'bike', image: BikeImage, sound: BikeSound},
+  {name: 'helicopter', image: HelicopterImage, sound: HelicopterSound},
+  {name: 'boat', image: BoatImage, sound: BoatSound},
   
 ];
 
