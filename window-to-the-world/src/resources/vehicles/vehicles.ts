10000788352866908f08e0b6d0aa899835c6f06d
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2022-11-01 11:51:12 +0100

    Last vehicle + style change for large screens

--- window-to-the-world/src/resources/vehicles/vehicles.ts
+++ window-to-the-world/src/resources/vehicles/vehicles.ts
@@ -3,12 +3,14 @@ import TrainImage from './images/train.jpg';
 import BikeImage from './images/bike.jpg';
 import HelicopterImage from './images/helicopter.jpg';
 import BoatImage from './images/boat.jpg';
+import AmbulanceImage from './images/ambulance.jpg';
 
 import CarSound from './sounds/car.mp3';
 import TrainSound from './sounds/train.mp3';
 import BikeSound from './sounds/bike.mp3';
 import HelicopterSound from './sounds/helicopter.mp3';
 import BoatSound from './sounds/boat.mp3';
+import AmbulanceSound from './sounds/ambulance.mp3';
 
 import {CardItem} from '../resourcesMaster';
 
@@ -18,6 +20,7 @@ const vehicles: CardItem[] = [
   {name: 'bike', image: BikeImage, sound: BikeSound},
   {name: 'helicopter', image: HelicopterImage, sound: HelicopterSound},
   {name: 'boat', image: BoatImage, sound: BoatSound},
+  {name: 'ambulance', image: AmbulanceImage, sound: AmbulanceSound},
   
 ];
 
