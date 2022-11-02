2bd152d08ebf47b28dd338fa3ae68454d3d40a31
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2022-11-02 13:15:13 +0100

    styling changes and guitar added

--- window-to-the-world/src/resources/instruments/instruments.ts
+++ window-to-the-world/src/resources/instruments/instruments.ts
@@ -1,12 +1,15 @@
 import PianoImage from './images/piano.jpg';
+import GuitarImage from './images/guitar.jpg';
 
 import PianoSound from './sounds/piano.mp3';
+import GuitarSound from './sounds/guitar.mp3';
 
 
 import {CardItem} from '../resourcesMaster';
 
 const vehicles: CardItem[] = [
   {name: 'piano', image: PianoImage, sound: PianoSound},
+  {name: 'guitar', image: GuitarImage, sound: GuitarSound},
   
 ];
 
