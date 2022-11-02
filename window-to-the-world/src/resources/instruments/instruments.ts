e5c10993fa70e2fdbadb3796857d67ac12d77623
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2022-11-02 11:57:34 +0100

    instruments category started

--- window-to-the-world/src/resources/instruments/instruments.ts
+++ window-to-the-world/src/resources/instruments/instruments.ts
@@ -1,7 +1,12 @@
+import PianoImage from './images/piano.jpg';
+
+import PianoSound from './sounds/piano.mp3';
+
+
 import {CardItem} from '../resourcesMaster';
 
 const vehicles: CardItem[] = [
-  
+  {name: 'piano', image: PianoImage, sound: PianoSound},
   
 ];
 
--- window-to-the-world/src/resources/resourcesMaster.ts
+++ window-to-the-world/src/resources/resourcesMaster.ts
@@ -1,8 +1,10 @@
 import animals from "./animals/Animals";
 import vehicles from "./vehicles/vehicles";
+import instruments from "./instruments/instruments";
 
 import animalsIcon from "./animals/images/animals-icon.svg";
 import vehiclesIcon from "./vehicles/images/vehicles-icon.svg";
+import instrumentsIcon from "./instruments/images/instruments-icon.svg";
 
 export interface CardItem {
   name: string;
@@ -12,6 +14,9 @@ export interface CardItem {
 
 const allCategories = [
   {name: 'animals', icon: animalsIcon, array: animals},
-  {name: 'vehicles', icon: vehiclesIcon, array: vehicles}];
+  {name: 'vehicles', icon: vehiclesIcon, array: vehicles},
+  {name: 'instruments', icon: instrumentsIcon, array: instruments},
+
+];
 
 export default allCategories;
